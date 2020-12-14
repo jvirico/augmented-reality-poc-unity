db01538de56d2aacf7f0761b1597d94031ae4990
jvirico <jvirico@gmail.com>
2020-12-14 20:06:22 +0100

    marker implemented

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,36 +9,28 @@ public class InputManager : MonoBehaviour
     //[SerializeField] private GameObject arObj;
     [SerializeField] private Camera arCam;
     [SerializeField] private ARRaycastManager _raycastManager;
+    [SerializeField] private GameObject crosshair;
+
     private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
 
     private Touch touch; // for mobile
+    private Pose pose;
 
     // Start is called before the first frame update
-    void Start()
-    {
-
-
-    }
+    void Start(){}
 
     // Update is called once per frame
     void Update()
     {
-
+      CrosshairCalculation();
       touch = Input.GetTouch(0);
 
       if(Input.touchCount<0 || touch.phase!=TouchPhase.Began)
         return;
 
-
       if(IsPointerOverUI(touch)) return;
 
-
-      Ray ray = arCam.ScreenPointToRay(touch.position);
-      if(_raycastManager.Raycast(ray, _hits)) // returns true if we hit anything
-      {
-        Pose pose = _hits[0].pose;
-        Instantiate(DataHandler.Instance.furniture, pose.position, pose.rotation);
-      }
+      Instantiate(DataHandler.Instance.furniture, pose.position, pose.rotation);
     }
 
     bool IsPointerOverUI(Touch touch)
@@ -48,5 +40,18 @@ public class InputManager : MonoBehaviour
       List<RaycastResult> results = new List<RaycastResult>();
       EventSystem.current.RaycastAll(eventData, results);
       return results.Count > 0;
+    }
+
+    void CrosshairCalculation()
+    {
+        Vector3 origin = arCam.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0)); //Vector3(0.5f, 0.5f, 0) gives us the center location of the screen!
+
+        Ray ray = arCam.ScreenPointToRay(origin);
+        if(_raycastManager.Raycast(ray, _hits)) // returns true if we hit anything
+        {
+            pose = _hits[0].pose;
+            crosshair.transform.position = pose.position;
+            crosshair.transform.eulerAngles = new Vector3(90,0,0);
+        }
     }
 }
